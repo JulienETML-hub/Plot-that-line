61dc531ad60c510e27b14b8da395bec58da782a3
JulienETML-hub <119680240+JulienETML-hub@users.noreply.github.com>
2024-10-22 16:42:52 +0200

    feat (addCity) Ajout formulaire pour ajouter une ville
    Ce formulaire permet d'ajouter une ville en lui donnant son nom, son pays, sa latitude et sa longitude. (Il y a eu quelque problèmes avec le gitignore c'est pour cela que ce commit viens tardivement, ces fichiers étaient ignoré avant car en essayant de régler mes problèmes de gitignore j'avais sans m'en rendre compte ignorer tout les nouveaux fichier .cs, qui sont évidemment des fichiers à ne pas ignorer puisqu'on travail dessus pour certains)

---
+++ Code/PlotThatLine2/addCity.Designer.cs
@@ -0,0 +1,110 @@
+﻿namespace PlotThatLine2
+{
+    partial class addCity
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            nameOfCity = new TextBox();
+            latitudeOfCity = new TextBox();
+            longitudeOfCity = new TextBox();
+            validate = new Button();
+            CountryOfCity = new TextBox();
+            SuspendLayout();
+            // 
+            // nameOfCity
+            // 
+            nameOfCity.Location = new Point(316, 109);
+            nameOfCity.Name = "nameOfCity";
+            nameOfCity.Size = new Size(160, 23);
+            nameOfCity.TabIndex = 0;
+            nameOfCity.Text = "Insérez le nom de la ville";
+            nameOfCity.TextAlign = HorizontalAlignment.Center;
+            nameOfCity.TextChanged += nameOfCity_TextChanged;
+            // 
+            // latitudeOfCity
+            // 
+            latitudeOfCity.Location = new Point(316, 203);
+            latitudeOfCity.Name = "latitudeOfCity";
+            latitudeOfCity.Size = new Size(150, 23);
+            latitudeOfCity.TabIndex = 1;
+            latitudeOfCity.Text = "Insérez la latitude";
+            latitudeOfCity.TextChanged += latitudeOfCity_TextChanged;
+            // 
+            // longitudeOfCity
+            // 
+            longitudeOfCity.Location = new Point(316, 241);
+            longitudeOfCity.Name = "longitudeOfCity";
+            longitudeOfCity.Size = new Size(150, 23);
+            longitudeOfCity.TabIndex = 2;
+            longitudeOfCity.Text = "Insérez la longitude";
+            longitudeOfCity.TextChanged += longitudeOfCity_TextChanged;
+            // 
+            // validate
+            // 
+            validate.Location = new Point(363, 299);
+            validate.Name = "validate";
+            validate.Size = new Size(75, 23);
+            validate.TabIndex = 3;
+            validate.Text = "Valider";
+            validate.UseVisualStyleBackColor = true;
+            validate.Click += validate_Click;
+            // 
+            // CountryOfCity
+            // 
+            CountryOfCity.Location = new Point(338, 150);
+            CountryOfCity.Name = "CountryOfCity";
+            CountryOfCity.Size = new Size(100, 23);
+            CountryOfCity.TabIndex = 4;
+            CountryOfCity.Text = "Insérez le pays";
+            CountryOfCity.TextChanged += CountryOfCity_TextChanged;
+            // 
+            // addCity
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(CountryOfCity);
+            Controls.Add(validate);
+            Controls.Add(longitudeOfCity);
+            Controls.Add(latitudeOfCity);
+            Controls.Add(nameOfCity);
+            Name = "addCity";
+            Text = "addCity";
+            Load += addCity_Load;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private TextBox nameOfCity;
+        private TextBox latitudeOfCity;
+        private TextBox longitudeOfCity;
+        private Button validate;
+        private TextBox CountryOfCity;
+    }
+}
---
+++ Code/PlotThatLine2/addCity.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Input;
+
+namespace PlotThatLine2
+{
+    public partial class addCity : Form
+    {
+        public event Action<City> CityAdded;  // Déclare l'événement
+        public string name;
+        public string country;
+        public double latitude;
+        public double longitude;
+
+
+
+        public addCity()
+        {
+            InitializeComponent();
+        }
+
+        private void nameOfCity_TextChanged(object sender, EventArgs e)
+        {
+            this.name = nameOfCity.Text;
+        }
+
+
+
+        private void CountryOfCity_TextChanged(object sender, EventArgs e)
+        {
+            this.country = CountryOfCity.Text;
+        }
+        private void latitudeOfCity_TextChanged(object sender, EventArgs e)
+        {
+            this.latitude = Convert.ToDouble(latitudeOfCity.Text);
+        }
+
+        private void longitudeOfCity_TextChanged(object sender, EventArgs e)
+        {
+            this.longitude = Convert.ToDouble(longitudeOfCity.Text);
+        }
+        private void validate_Click(object sender, EventArgs e)
+        {
+            City addedCity = new City(this.name, this.country, this.latitude, this.longitude);
+            closeAddCity(addedCity);
+        }
+        private void closeAddCity(City city)
+        {
+            CityAdded?.Invoke(city);
+            this.Close();
+        }
+
+        private void addCity_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
