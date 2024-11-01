f235afd3f1beaa71d8eb45493eda7621087beb17
JulienETML-hub <119680240+JulienETML-hub@users.noreply.github.com>
2024-11-01 20:32:25 +0100

    Tests : (City) Ajout tests unitaires
    Test unitaire s'occupant des méthodes de changement de propriété d'un objet city (changeName, changeCountry) ainsi qu'un test unitaire s'occupant d'un constructor.

---
+++ Code/PlotThatLine2Tests/CityTests.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PlotThatLine2;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlotThatLine2.Tests
+{
+    [TestClass()]
+    public class CityTests
+    {
+        [TestMethod()]
+        public void changeCountryTest()
+        {
+            // Arrange
+            City city = new City("Lausanne", "Suise", 45, 5);
+
+            // Act
+            city.changeCountry("Suisse");
+
+            // Assert
+            Assert.IsTrue(city.Country is string);
+            Assert.AreEqual("Suisse", city.Country);
+        }
+
+        [TestMethod()]
+        public void changeNameTest()
+        {
+            // Arrange
+            City city = new City("Pully2", "Suise", 45, 5);
+
+            // Act
+            city.changeName("Pully");
+
+            // Assert
+            Assert.IsTrue(city.Name is string);
+            Assert.AreEqual("Pully", city.Name);
+        }
+        [TestMethod]
+        public void Constructor_InitializesPropertiesCorrectly()
+        {
+            // Arrange & Act
+            City city = new City("Lausanne", "Suisse", 45, 5);
+
+            // Assert
+            Assert.AreEqual("Lausanne", city.Name);
+            Assert.AreEqual("Suisse", city.Country);
+            Assert.AreEqual(45, city.Latitude);
+            Assert.AreEqual(5, city.Longitude);
+        }
+        
+
+    }
+}
